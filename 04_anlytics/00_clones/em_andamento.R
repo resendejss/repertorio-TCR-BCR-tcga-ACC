a652ec5a06eaaee5bf0dd7ac1cdc0c67f5cce6ea
Jean Silva de Souza Resende <jean.souza@edu.unipar.br>
2024-04-29 23:31:55 -0300

    update

--- 04_anlytics/00_clones/em_andamento.R
+++ 04_anlytics/00_clones/em_andamento.R
@@ -62,3 +62,66 @@ clust_TCR$`130723_UNC9-SN296_0386_BC2E4WACXX_TAGCTT_L006_report`
 
 clust_TCR$`130723_UNC9-SN296_0386_BC2E4WACXX_ACTTGA_L003_report`
 
+library(dplyr)
+
+# funcao para agrupar os dataframes
+removendoi_NA <- function(df){
+  df %>%
+    filter(!rowSums(is.na(.)) == ncol(.))
+}
+
+agrupar_por_cluster <- function(df){
+  df = df[!is.na(df$Clone),]
+  df %>%
+    group_by(Cluster) %>%
+    mutate(Clones = sum(Clones),
+           Proportion = sum(Proportion)) %>%
+    ungroup() %>%
+    distinct(Cluster, .keep_all = TRUE) %>%
+    as.data.frame()
+}
+
+
+teste_TCR <- lapply(clust_TCR, removendoi_NA)
+teste_BCR <- lapply(clust_BCR, removendoi_NA)
+
+clust_TCR_agrupados <- lapply(teste_TCR, agrupar_por_cluster)
+clust_BCR_agrupados <- lapply(teste_BCR, agrupar_por_cluster)
+
+
+rownames(immdata$data$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`)
+table(is.na(immdata$data$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Clones))
+
+# aplicando a funcao na lista de dataframes
+clust_TCR_agrupados <- lapply(clust_TCR, agrupar_por_cluster)
+clust_BCR_agrupados <- lapply(clust_BCR, agrupar_por_cluster)
+
+length(clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster)
+length(unique(clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster))
+
+length(clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster)
+length(unique(clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster))
+
+clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`
+clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`
+
+
+table(clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster)
+
+clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`[
+  clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster == "IGHV1-3/IGHJ4_length_24",]
+
+clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`[
+  clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_GATCAG_L005_report`$Cluster == "IGHV1-3/IGHJ4_length_24",]
+
+
+clust_TCR$`130723_UNC9-SN296_0386_BC2E4WACXX_ATCACG_L003_report`$CDR3.nt[1]
+clust_TCR$`130723_UNC9-SN296_0386_BC2E4WACXX_ATCACG_L003_report`$CDR3.nt[1]
+
+clust_BCR_agrupados$`130723_UNC9-SN296_0386_BC2E4WACXX_ACTTGA_L003_report`
+clust_BCR$`130723_UNC9-SN296_0386_BC2E4WACXX_ATCACG_L003_report`
+
+
+
+
+
